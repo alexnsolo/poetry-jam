6ac247c0c287f8faa64e8c35b8ed2f8993fc326a
Alex Solo <alex.n.solo@gmail.com>
2015-12-05 19:43:31 -0800

    inserting poems securely via client

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -1,10 +1,17 @@
-angular.module('poetry-jam').controller('HomeCtrl', function($scope, $state, $meteor) {
+angular.module('poetry-jam').controller('HomeCtrl', function($scope, $rootScope, $state, $meteor) {
 	$scope.$meteorSubscribe('Poems');
 	$scope.poems = $scope.$meteorCollection(Poems);
 
 	$scope.createNewPoem = function() {
-		$meteor.call('CreateNewPoem').then(function(poem) {
-			$state.go('poem', {poemId: poem._id});
-		});
+		if (!$rootScope.currentUser) {
+			return;
+		}
+
+		var poem = {};
+		poem.name = '';
+		poem.author = $rootScope.currentUser.email;
+		poem.owner = $rootScope.currentUser._id;
+		poem._id = Poems.insert(poem);
+		$state.go('poem', {poemId: poem._id});
 	};
 });
